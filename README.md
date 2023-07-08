f58232a4fc7ae30388d7b9f96b93e6e63fc1c21a
Hawa <hawamaria33@gmail.com>
2023-07-09 00:48:05 +0300

    Initialize gh-pages branch

---
+++ README.md
@@ -0,0 +1,42 @@
+# Blog-project
+# Blog Application
+This is a blog application that allows users to create and manage blog posts. It provides functionality for creating new blog posts, displaying existing blog posts, deleting blog posts, and viewing individual blog posts in detail.
+
+# Features
+- Create new blog posts by filling out a form with a title, author name, blog body, and optional image upload link.
+- Display a list of existing blog posts with their titles, authors, and brief previews.
+- Click on a blog post title to view the full content of the blog post.
+- Delete blog posts using the delete button associated with each post.
+
+# Technologies Used
+HTML
+CSS
+JavaScript
+Fetch API
+JSON Server (to simulate a REST API for storing and retrieving blog posts)
+
+# Getting Started
+To run this application locally, follow these steps:
+
+- Clone the repository: git clone https://github.com/your-username/blog-application.git
+- Change to the project directory: cd blog-application
+- Start the JSON Server: json-server --watch db.json
+- Open the application in a web browser by opening the index.html file.
+
+
+# Contributing
+Contributions are welcome! If you'd like to contribute to this project, please follow these steps:
+
+- Fork the repository.
+- Create a new branch for your feature: git checkout -b feature-name
+- Make your changes and commit them: git commit -m 'Add some feature'
+- Push the changes to your fork: git push origin feature-name
+- Submit a pull request describing your changes.
+
+# License
+This project is licensed under the MIT License.
+
+# Contact
+If you have any questions or suggestions, please feel free to reach out to me at hawamaria@gmail.com
+
+Feel free to modify the README template according to your specific project requirements and add any additional sections or information you think would be helpful.
---
+++ index.js
@@ -0,0 +1,176 @@
+const container = document.querySelector(".container");
+
+const renderposts = async () => {
+  const url = "http://localhost:3000/Blogs";
+  const res = await fetch(url);
+  const blogs = await res.json();
+
+  let template = "";
+  blogs.forEach((blog) => {
+    template += `
+    <div class="blog">
+    
+        <h2 class="blog-title">
+        
+          <img class="images" src="${blog.image}" alt="Blog Image">${blog.title} </img>
+        
+        </h2>
+     
+        <button class="delete" data-blog-id="${blog.id}">Delete</button>
+        <p class="blog-body">${blog.body}</p>
+        <p class="author">  Author:${blog.author} </p>
+       
+      </div>    
+    `;
+  });
+
+  container.innerHTML = template;
+
+  //handle click events on blog titles
+  handleBlogTitleClicks();
+  function handleBlogTitleClicks() {
+    const blogTitles = document.querySelectorAll(".blog-title");
+
+    blogTitles.forEach((title) => {
+      const blogBody = title.nextElementSibling;
+
+      blogBody.style.display = "none";
+      const blogAuthor = blogBody.nextElementSibling;
+      blogAuthor.style.display = "none";
+      const deletebtn = blogAuthor.nextElementSibling;
+      deletebtn.style.display = "none";
+
+      title.addEventListener("click", () => {
+        const content = document.querySelector(".content");
+
+        content.innerHTML = "";
+
+        content.appendChild(title);
+        content.appendChild(blogBody);
+        content.appendChild(blogAuthor);
+        content.appendChild(deletebtn);
+        blogBody.style.display = "block";
+        blogAuthor.style.display = "block";
+        deletebtn.style.display = "block";
+        blogTitles.forEach((t) => {
+          t.style.display = t === title ? "block" : "none";
+        });
+      });
+    });
+  }
+
+  function goToHomePage() {
+    const homePage = document.querySelector("#home");
+    homePage.addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
+  }
+
+  //navigate to homepage
+  goToHomePage();
+  function handleBlogClick() {
+    const section = document.querySelector(".section");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+      <div class="form">
+        <form id="blogForm">
+          <label for="title">Blog Title:</label>
+          <input type="text" id="blogTitle" name="title" required>
+  
+          <label for="author">Author's Name:</label>
+          <input type="text" id="authorName" name="author" required>
+          <br>
+          <br>
+          <label for="body">Blog Body:</label>
+          <textarea id="blogBody" name="body" required></textarea>
+          <br>
+          <label for="image">Image:</label>
+        <input  id="image" name="image" accept="image/*">
+         <br>
+
+          <button type="submit" id="submit">submit</button>
+        </form>
+      </div>
+    `;
+
+    newDiv.classList.add("blog-form");
+    section.innerHTML = "";
+    section.appendChild(newDiv);
+    handleSubmit();
+  }
+  const blog = document.querySelector("#blog");
+  blog.addEventListener("click", handleBlogClick);
+
+  //handle form submission
+  function handleSubmit() {
+    const submitBtn = document.querySelector("#submit");
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const title = form.elements.title.value;
+      const author = form.elements.author.value;
+      const body = form.elements.body.value;
+      const image = form.elements.image.value;
+
+      fetch(url, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          title,
+          author,
+          body,
+          image,
+        }),
+      });
+    });
+  }
+
+  //handles delete button
+  function handleDeleteButtonClicks() {
+    const deleteButtons = document.querySelectorAll(".delete");
+
+    deleteButtons.forEach((deleteButton) => {
+      deleteButton.addEventListener("click", () => {
+        const blogId = deleteButton.dataset.blogId;
+        deleteBlogPost(blogId);
+      });
+    });
+  }
+  //deletes blog post
+  handleDeleteButtonClicks();
+  function deleteBlogPost(blogId) {
+    const deleteUrl = `${url}/${blogId}`;
+
+    fetch(deleteUrl, {
+      method: "DELETE",
+    });
+  }
+ // Shows "Coming Soon" alert on "More" navigation link click
+  function comingSoonAlert() {
+    Swal.fire({
+      title: "Coming Soon",
+      text: "Stay tuned for more updates!",
+      icon: "info",
+      confirmButtonText: "OK",
+    });
+  }
+
+    // Shows "Subscribe" success alert on button click
+  function subscribeAlert() {
+    Swal.fire({
+      title: "Subscribe",
+      text:"You have successfully subscribed!",
+      icon: "success",
+      confirmButtonText: "OK",
+    });
+  }
+
+  const moreLink = document.querySelector("#more");
+  moreLink.addEventListener("click", comingSoonAlert);
+
+  const subscribeBtn = document.getElementById("subscribeBtn");
+  subscribeBtn.addEventListener("click", subscribeAlert);
+};
+window.addEventListener("DOMContentLoaded", renderposts);
